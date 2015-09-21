dad7379373ff7ed7ddbbd861ea5c12bbe3bceda2
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-09-21 15:15:20 -0600

    home route with controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  get '/auth/facebook/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  get  '/dashboard', to: 'dashboard#show'
+  root 'home#show'
 end
