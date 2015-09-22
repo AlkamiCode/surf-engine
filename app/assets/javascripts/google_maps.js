a575e8515c179e438cd6c660847eb8cee278993b
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-09-22 14:48:35 -0600

    add color to google map, add dropdown menu to nav-bar

--- app/assets/javascripts/google_maps.js
+++ app/assets/javascripts/google_maps.js
@@ -1,7 +1,50 @@
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+  var customMapType = new google.maps.StyledMapType([
+      {
+        stylers: [
+          {hue: '#70BBB5'},
+          {visibility: 'simplified'},
+          {gamma: 0.5},
+          {weight: 0.5}
+        ]
+      },
+      {
+        elementType: 'labels',
+        stylers: [{visibility: 'off'}]
+      },
+      {
+        featureType: 'water',
+        stylers: [{color: '#70BBB5'}]
+      }
+    ], {
+      name: 'Custom Style'
   });
+  var customMapTypeId = 'custom_style';
+
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: {lat: 12.625111, lng: -87.354444}
+  });
+
+  var kmlLayer = new google.maps.KmlLayer({
+
+    suppressInfoWindows: true,
+    map: map
+  });
+
+  kmlLayer.addListener('click', function(kmlEvent) {
+    var text = kmlEvent.featureData.description;
+    showInContentWindow(text);
+  });
+
+  function showInContentWindow(text) {
+    var sidediv = document.getElementById('content-window');
+    sidediv.innerHTML = text;
+  }
+
+
+
+  map.mapTypes.set(customMapTypeId, customMapType);
+  map.setMapTypeId(customMapTypeId);
 }
