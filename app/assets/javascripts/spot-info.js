82b6ba5a172ff8bcfa1a82a83ca3998bb3357a34
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-09-25 20:24:49 -0600

    rough draft of spot-info-table

--- app/assets/javascripts/spot-info.js
+++ app/assets/javascripts/spot-info.js
@@ -5,8 +5,9 @@ function add_handler(spot) {
     $.ajax({
       type: "GET",
       url:  "http://localhost:3000/spot/" + this.id,
+      // url:  "https://surfiesta.herokuapp.com/spot/" this.id,
       success: function(spot){
-        $("#spot-info-field").append("<h1>'"+spot.name+"'</h1>")
+        $("#spot-info-field").append("")
       }
     })
   });
@@ -16,10 +17,17 @@ $(document).ready(function(){
   $.ajax({
     type: "GET",
     url:  "http://localhost:3000/spots",
+    // url:  "https://surfiesta.herokuapp.com/spots",
     success: function(spots){
       dom_spots = $(".spots");
       $.each(spots, function(index, spot){
-        var raw_dom_spot = "<div class='spot' id='"+spot.id+"' data-name='"+spot.name+"'><a class='spot-name'>"+spot.name+"</a></div>"
+        var raw_dom_spot = "<div class='spot' id='"
+                            + spot.id
+                            + "' data-name='"
+                            + spot.name
+                            + "'><a class='spot-name'>"
+                            + spot.name
+                            + "</a></div>"
         dom_spots.append(raw_dom_spot)
         var dom_spot = dom_spots.children().last()
         add_handler(dom_spot)
