dddd6ca242a013c9234161a4ea192e1656ac3864
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-09-27 12:20:34 -0600

    table appended to each accordion element

--- app/assets/javascripts/spot-info.js
+++ app/assets/javascripts/spot-info.js
@@ -13,17 +13,18 @@ function add_handler(spot) {
 }
 
 $(document).ready(function(){
+  // $('#accordion').append("<h3><a href='#'>NEW TAB</a></h3><div><p>Check Out This Data</p></div>");
   $.ajax({
     type: "GET",
     url:  "http://localhost:3000/spots",
     // url:  "https://surfiesta.herokuapp.com/spots",
     success: function(spots){
       dom_spots = $(".spots");
-
       $.each(spots, function(index, spot){
-        // var raw_dom_spot = "<h3><a href='#'>NewParagraph</a></h3><div><p>Newdata</p></div>"
-        var raw_dom_spot = "<div class='spot' id='"+spot.id+"' data-name='"+spot.name+"'><a class='spot-name'>"+spot.name+"</a></div>"
-        // var raw_dom_spot = "<div class='spot' id='"+spot.id+"' data-name='"+spot.name+"'><h3>Section1</h3><div><p>Mauris mauris ante, blandit et, ultrices a, susceros. Nam mi. Proin viverra leo ut odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.</p></div>"
+        // var raw_dom_spot = "<h3><a href='#'>NEW TAB</a></h3><div><p>Check Out This Data</p></div>"
+        // var raw_dom_spot = "<h3 class='spot' data-name='"+spot.name+"'>"+spot.name+"</h3><div class='spot' id='"+spot.id+"' data-name='"+spot.name+"'><p>Check Out This Data</p></div>"
+        var raw_dom_spot = "<h6 class='spot' data-name='"+spot.name+"'>"+spot.name+"</h6><div><table class='table table-condensed'><tr><td>Wave Quality</td><td>"+spot.wave_quality+"</td></tr><tr><td>Experience Level</td><td>"+spot.experience_level+"</td></tr><tr><td>Frequency</td><td>"+spot.frequency+"</td></tr><tr><td>Break Type</td><td>"+spot.break_type+"</td></tr><tr><td>Direction</td><td>"+spot.direction+"</td></tr><tr><td>Sea Floor</td><td>"+spot.sea_floor+"</td></tr><tr><td>Power</td><td>"+spot.power+"</td></tr><tr><td>Usual Length</td><td>"+spot.usual_length+"</td></tr><tr><td>Epic Length</td><td>"+spot.epic_length+"</td></tr><tr><td>Ideal Swell Direction</td><td>"+spot.ideal_swell_direction+"</td></tr><tr><td>Ideal Wind Direction</td><td>"+spot.ideal_wind_direction+"</td></tr><tr><td>Swell Size</td><td>"+spot.swell_size+"</td></tr><tr><td>Ideal Tide</td><td>"+spot.ideal_tide+"</td></tr><tr><td>Ideal Tide Movement</td><td>"+spot.ideal_tide_movement+"</td></tr><tr><td>Weekday Crowd</td><td>"+spot.weekday_crowd+"</td></tr><tr><td>WeekEnd Crowd</td><td>"+spot.weekend_crowd+"</td></tr><tr><td>Dangers</td><td>"+spot.dangers+"</td></tr><tr><td>Sub Zone</td><td>"+spot.sub_zone+"</td></tr></table></div>"
+
         dom_spots.append(raw_dom_spot)
         var dom_spot = dom_spots.children().last()
         add_handler(dom_spot)
@@ -31,5 +32,5 @@ $(document).ready(function(){
       dom_spots.accordion("refresh");
     }
   })
-  add_handler($(".spot"))
+  // add_handler($(".spot"))
 });
--- app/assets/javascripts/spots-filter.js
+++ app/assets/javascripts/spots-filter.js
@@ -2,13 +2,14 @@ $(document).ready(function () {
 
 $('#spot_filter_name').on('keyup', function () {
     var currentName = this.value.toLowerCase();
-
     $('.spot').each(function (index, spot) {
       $spot = $(spot);
       if ($spot.data('name').toString().toLowerCase().indexOf(currentName) !== -1) {
         $spot.show();
+        $spot.nextAll().show();
       } else {
         $spot.hide();
+        $spot.nextAll().hide();
       }
     });
   });
