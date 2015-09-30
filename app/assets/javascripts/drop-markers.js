7780d07f0456bfeb073ded6e29853c35c69cb763
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-09-30 02:05:27 -0600

    tidy up, pimp shit out

--- app/assets/javascripts/drop-markers.js
+++ app/assets/javascripts/drop-markers.js
@@ -2,8 +2,8 @@ var image = 'assets/24-px-surf-icon.png';
 
 function add_handler(spot) {
   spot.on("click", function(){
-    console.log(spot.data('lat'))
-    console.log(spot.data('lon'))
+    // console.log(spot.data('lat'))
+    // console.log(spot.data('lon'))
     position = {lat: spot.data('lat'), lng: spot.data('lon')}
     clearMarkers();
     addMarkerWithTimeout(position, 1000)
--- app/assets/javascripts/spot-info.js
+++ app/assets/javascripts/spot-info.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(".region").on("click", function(){
-    $('#search-hand').hide();
+    $('#search-hand').slideUp('slow');
     var currentRegion = this.text;
 
     $.ajax({
@@ -10,7 +10,7 @@ $(document).ready(function(){
         dom_spots = $(".spots");
         console.log("Fetched all spots!")
         spots.forEach(function(spot){
-          var raw_dom_spot = "<h3 class='spot' data-experience="+spot.experience_level+" data-lat="+spot.latitude+" data-lon="+spot.longitude+" data-name='"+spot.name+"'>"+spot.name+"</h3><div><table class='table table-condensed'><tr><td>Wave Quality</td><td>"+spot.wave_quality+"</td></tr><tr><td>Experience Level</td><td>"+spot.experience_level+"</td></tr><tr><td>Frequency</td><td>"+spot.frequency+"</td></tr><tr><td>Break Type</td><td>"+spot.break_type+"</td></tr><tr><td>Direction</td><td>"+spot.direction+"</td></tr><tr><td>Sea Floor</td><td>"+spot.sea_floor+"</td></tr><tr><td>Power</td><td>"+spot.power+"</td></tr><tr><td>Usual Length</td><td>"+spot.usual_length+"</td></tr><tr><td>Epic Length</td><td>"+spot.epic_length+"</td></tr><tr><td>Ideal Swell Direction</td><td>"+spot.ideal_swell_direction+"</td></tr><tr><td>Ideal Wind Direction</td><td>"+spot.ideal_wind_direction+"</td></tr><tr><td>Swell Size</td><td>"+spot.swell_size+"</td></tr><tr><td>Ideal Tide</td><td>"+spot.ideal_tide+"</td></tr><tr><td>Ideal Tide Movement</td><td>"+spot.ideal_tide_movement+"</td></tr><tr><td>Weekday Crowd</td><td>"+spot.weekday_crowd+"</td></tr><tr><td>WeekEnd Crowd</td><td>"+spot.weekend_crowd+"</td></tr><tr><td>Dangers</td><td>"+spot.dangers+"</td></tr><tr><td>Sub Zone</td><td>"+spot.sub_zone+"</td></tr></table></div>"
+          var raw_dom_spot = "<h3 class='spot' data-experience="+spot.experience_level+" data-lat="+spot.latitude+" data-lon="+spot.longitude+" data-name='"+spot.name+"'>"+spot.name+"</h3><div><table class='table table-condensed'><tr><td>Wave Quality</td><td>"+spot.wave_quality+"</td></tr><tr><td>Experience Level</td><td>"+spot.experience_level+"</td></tr><tr><td>Frequency</td><td>"+spot.frequency+"</td></tr><tr><td>Break Type</td><td>"+spot.break_type+"</td></tr><tr><td>Direction</td><td>"+spot.direction+"</td></tr><tr><td>Sea Floor</td><td>"+spot.sea_floor+"</td></tr><tr><td>Power</td><td>"+spot.power+"</td></tr><tr><td>Usual Length</td><td>"+spot.usual_length+"</td></tr><tr><td>Epic Length</td><td>"+spot.epic_length+"</td></tr><tr><td>Ideal Swell Direction</td><td>"+spot.ideal_swell_direction+"</td></tr><tr><td>Ideal Wind Direction</td><td>"+spot.ideal_wind_direction+"</td></tr><tr><td>Swell Size</td><td>"+spot.swell_size+"</td></tr><tr><td>Ideal Tide</td><td>"+spot.ideal_tide+"</td></tr><tr><td>Ideal Tide Movement</td><td>"+spot.ideal_tide_movement+"</td></tr><tr><td>Weekday Crowd</td><td>"+spot.weekday_crowd+"</td></tr><tr><td>WeekEnd Crowd</td><td>"+spot.weekend_crowd+"</td></tr><tr><td>Dangers</td><td>"+spot.dangers+"</td></tr></table></div>"
           dom_spots.append(raw_dom_spot)
           var dom_spot = dom_spots.children().last().prev()
           add_handler(dom_spot)
