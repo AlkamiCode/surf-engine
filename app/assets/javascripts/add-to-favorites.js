4b27a55533944ac916389420a3ed9c5255dd787f
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-10-01 01:42:08 -0600

    toggle heart icon without triggering accordion

--- app/assets/javascripts/add-to-favorites.js
+++ app/assets/javascripts/add-to-favorites.js
@@ -1,5 +1,6 @@
 function add_like_handler(spot) {
   spot.find('.glyphicon').on("click", function(){
+    event.stopImmediatePropagation(this)
     $(this).toggleClass('glyphicon-heart-empty').toggleClass('glyphicon-heart');
   })
 };
